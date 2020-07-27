fa5cbb2c840e82ca23285c7e6f2d0e87048c84c8
云服务部-赖盛源 <756486059@qq.com>
2020-07-27 16:59:51 +0800

    no message

--- AYLogManager.podspec
+++ AYLogManager.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
 s.name         = "AYLogManager"
-s.version      = "1.0.6"
+s.version      = "1.0.7"
 s.summary      = "AYLogManager."
 s.description  = <<-DESC
 AYLogManager局域网内查看log
--- AYLogManager/AppDelegate.m
+++ AYLogManager/AppDelegate.m
@@ -16,7 +16,6 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
     
 //    [[AYHttpServerManager sharedInstance] startupServer];
     
